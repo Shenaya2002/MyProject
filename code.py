1fa2353843f0b4030a63d1b3df406efabe82b656
Shenaya2002 <shenaya.demel1@gmail.com>
2025-03-24 12:20:21 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,3 +2,13 @@ def check_balance(balance):
     """Displays the current account balance."""
     print("\nExecuting: check_balance()")
     print(f"Your current balance is: ${balance:.2f}")
+
+def deposit(balance, amount):
+    """Deposits money into the account."""
+    print("\nExecuting: deposit()")
+    if amount > 0:
+        balance += amount
+        print(f"${amount:.2f} deposited successfully!")
+    else:
+        print("Deposit amount must be greater than zero.")
+    return balance
